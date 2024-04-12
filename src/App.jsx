6356e537bd2a04f049e87e8333ee5653166f9fcd
AnSh202302 <125475748+AnSh202302@users.noreply.github.com>
2024-04-12 09:50:52 +0200

    add components,  events, props

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,38 @@
+import CoreConcept from "./components/CoreConcept";
+import Header from "./components/Header/Header.jsx";
+import TapButton from "./components/TapButton.jsx";
+import { CORE_CONCEPTS } from "./data";
+
 function App() {
+  let tapContent = "Please click!";
+  function handleClick(selectedButton) {
+    tapContent = selectedButton;
+  }
   return (
     <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]} />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TapButton onSelect={() => handleClick("components")}>
+              Components
+            </TapButton>
+            <TapButton onSelect={() => handleClick("jsx")}>JSX</TapButton>
+            <TapButton onSelect={() => handleClick("props")}>Props</TapButton>
+            <TapButton onSelect={() => handleClick("state")}>State</TapButton>
+          </menu>
+          {tapContent}
+        </section>
       </main>
     </div>
   );
---
+++ src/components/TapButton.jsx
@@ -0,0 +1,9 @@
+function TapButton({ children, onSelect }) {
+  return (
+    <li>
+      <button onClick={onSelect}>{children}</button>
+    </li>
+  );
+}
+
+export default TapButton;
